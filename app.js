4274ee9d83b3dd5328f8e1ce7ae3aa9cd9a9c31f
Eric Gómez <egomez_99@yahoo.com>
2021-06-08 10:03:42 -0500

    Inicio con Home.html

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const path=require('path');
 app.use(express.static('public'));
 app.listen(3000,()=>{console.log("Servidor corriendo en el puerto 3000")})
 
+
 app.get('/',(req,res)=>{
-    res.send('Hola inicia con /home');
-});
-app.get('/home',(req,res)=>{
     let htmlPath1=path.join(__dirname,'./views/home.html');
     res.sendFile(htmlPath1);
 });
