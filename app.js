a11ebdf19121c7d02b623275e90c2dd30ea04832
Eric Gómez <egomez_99@yahoo.com>
2021-06-07 21:27:03 -0500

    Modificacion al apps.js por la carpeta views

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const app=express();
+
 const path=require('path');
 
-const app=express();
+
 app.use(express.static('public'));
-app.listen(3000,()=>{console.log("Servido")})
+app.listen(3000,()=>{console.log("Servidor corriendo en el puerto 3000")})
+
 app.get('/',(req,res)=>{
-    res.send('Hola');});
+    res.send('Hola');
+});
 app.get('/home',(req,res)=>{
-    let htmlPath1=path.join(__dirname,'./index.html');
-    res.sendFile(htmlPath1);});
+    let htmlPath1=path.join(__dirname,'./views/index.html');
+    res.sendFile(htmlPath1);
+});
 app.get('/carrito',(req,res)=>{
-    let htmlPath2=path.join(__dirname,'./productCart.html');
-    res.sendFile(htmlPath2);});
+    let htmlPath2=path.join(__dirname,'./views/productCart.html');
+    res.sendFile(htmlPath2);
+});
 app.get('/detalle-producto',(req,res)=>{
-    let htmlPath3=path.join(__dirname,'./productDetail.html');
-    res.sendFile(htmlPath3);});
+    let htmlPath3=path.join(__dirname,'./views/productDetail.html');
+    res.sendFile(htmlPath3);
+});
 app.get('/registra',(req,res)=>{
-    let htmlPath4=path.join(__dirname,'./register.html');
-    res.sendFile(htmlPath4);});
+    let htmlPath4=path.join(__dirname,'./views/register.html');
+    res.sendFile(htmlPath4);
+});
