cdbdc1d372fcac1ecda3def241d315fdcf190108
adrian-cr <acr77frb@gmail.com>
2021-06-17 21:58:38 -0500

    updated js file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app=express();
 const path=require('path');
 
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname,'./Public')));
 app.listen(process.env.PORT || 3000,()=>{console.log("Servidor corriendo en el puerto 3000")});
 
 
