3e1c3e5dc74269b293a0e50fe155aad402ae417c
PauloEsau1 <pauloesau1696@hotmail.com>
2021-06-18 13:44:29 -0500

    heroku

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path=require('path');
 app.use(express.static('public'));
 app.listen(3000,()=>{console.log("Servidor corriendo en el puerto 3000")})
 
-
 app.get('/',(req,res)=>{
     let htmlPath1=path.join(__dirname,'./views/index.html');
     res.sendFile(htmlPath1);
